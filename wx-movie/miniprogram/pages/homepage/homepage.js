7e5132297d175a48a7ec6c0a2679cd4609f9add1
Gomader <gh970818@hotmail.com>
2019-07-11 01:12:42 +0900

    '热门电影页面动态布局搞定，还没写bindtap'

--- wx-movie/miniprogram/pages/homepage/homepage.js
+++ wx-movie/miniprogram/pages/homepage/homepage.js
@@ -1,67 +1,55 @@
-// miniprogram/pages/homepage/homepage.js
+
+const db = wx.cloud.database({
+  env: 'movies-udacity-f36f2'
+})
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    clink:'',
+    name:'',
+    movieid:''
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  onPullDownRefresh(){
+    this.getmovies()
+    wx.stopPullDownRefresh()
+  },
   onLoad: function (options) {
-    var a = Math.floor(Math.random()*219)
-    console.log(a)
+    this.getmovies()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getmovies(){
+    var a = Math.floor(Math.random()*10).toString()
+    wx.showLoading({
+      title: '数据正在加载中...',
+    })
+    db.collection('movies').where({
+      rand:a
+    }).get().then(result => {
+      wx.hideLoading()
+      var datas = result.data[0]
+      this.setData({
+        clink: datas.clink,
+        name: datas.name,
+        movieid:datas._id
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  getnew(){
+    this.getmovies()
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  tohotmovie(){
+    wx.navigateTo({
+      url: '/pages/hotmovie/hotmovie'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  tomine() {
+    wx.navigateTo({
+      url: '/pages/mine/mine'
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  tomovieinfo(a){
+    var id = a.currentTarget.dataset.id
+    wx.navigateTo({
+      url: '/pages/movieinfo/movieinfo?id=' + id
+    })
   }
 })
--- wx-movie/miniprogram/pages/hotmovie/hotmovie.js
+++ wx-movie/miniprogram/pages/hotmovie/hotmovie.js
@@ -1,66 +1,49 @@
-// miniprogram/pages/hotmovie/hotmovie.js
+
+const db = wx.cloud.database({
+  env: 'movies-udacity-f36f2'
+})
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    pagenum: 1,
+    movies:[]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    this.getmovielist()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
-
+    var times = this.data.pagenum + 1
+    this.setData({
+      pagenum: times
+    })
+    this.getnewovielist()
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  getnewovielist(){
+    wx.showLoading({
+      title: '数据正在加载中...',
+    })
+    var pn = this.data.pagenum*5
+    db.collection('movies').skip(pn).limit(5).get().then(result => {
+      wx.hideLoading()
+      var datas = result.data
+      const data = this.data.movies.concat(datas)
+      this.setData({
+        movies:data
+      })
+    })
+  },
+  getmovielist() {
+    wx.showLoading({
+      title: '数据正在加载中...',
+    })
+    var pn = this.data.pagenum * 5
+    db.collection('movies').limit(5).get().then(result => {
+      wx.hideLoading()
+      var datas = result.data
+      this.setData({
+        movies: datas
+      })
+    })
+  },
+  
 })
